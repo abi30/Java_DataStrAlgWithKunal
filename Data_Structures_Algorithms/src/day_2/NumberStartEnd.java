4a0fcf96ac28ce2951b441ab4727fe3a97b36869
abi30 <rakib0751@gmail.com>
2021-09-29 03:36:24 +0200

    good night Algo

--- Data_Structures_Algorithms/src/day_2/NumberStartEnd.java
+++ Data_Structures_Algorithms/src/day_2/NumberStartEnd.java
@@ -10,8 +10,8 @@ public class NumberStartEnd {
             int[] arr= {5,7,7,7,7,8,8,10};
 
             int target=7;
-            int[] ans=numberSearch(arr,target);
-            System.out.println(Arrays.toString(ans));
+            //int[] ans=numberSearch(arr,target);
+            System.out.println(Arrays.toString(numberSearch(arr,target)));
 
         }
 
@@ -34,31 +34,28 @@ public class NumberStartEnd {
 
     static  int search(int[] nums,int target, boolean findStartIndex){
 
+
         int ans=-1;
         int start=0;
-        int end =nums.length-1;
+        int end = nums.length-1;
 
-        while(start<=end) {
-
-            int mid=start+(end-start)/2;
-
-            if(target<nums[mid]) {
+        while (start<=end){
+        int mid= start+(end-start)/2;
+            if(target<nums[mid]){
                 end=mid-1;
-            }else if(target>nums[mid]){
+            }else if (target>nums[mid]){
                 start=mid+1;
             }else {
-                // potation ans is found
-                ans = mid;
-                if(findStartIndex==true){
+                // possible ans is found
+                ans=mid;
+                if(findStartIndex){
                     end=mid-1;
                 }else {
                     start=mid+1;
                 }
             }
-
         }
 
         return  ans;
-
     }
 }
